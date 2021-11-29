bad244092f139f3e2c4675c3a049ad15147be5d1
Nick Chen <nickc@Nicks-MacBook-Air.local>
2021-11-29 15:46:59 -0500

    first

---
+++ build/precache-manifest.80900629e3460e46d9fe556b21e86ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41703c93318960dd8d5db4c53e15cbdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8fe90e247c1a2d79c54",
+    "url": "/static/css/main.eb93b836.chunk.css"
+  },
+  {
+    "revision": "4b5a0659b690fe5c44a5",
+    "url": "/static/js/2.107f0c64.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.107f0c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8fe90e247c1a2d79c54",
+    "url": "/static/js/main.053b156e.chunk.js"
+  },
+  {
+    "revision": "f55b84c65c092ec71c90",
+    "url": "/static/js/runtime-main.bcab16a1.js"
+  }
+]);
